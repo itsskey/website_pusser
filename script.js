4f47666a87b1f992a9c1e6189c18f797d0c9a161
Karina Aitynbetova <109790338+itsskey@users.noreply.github.com>
2026-02-18 17:07:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('start-box').addEventListener('click', function() {
+    window.location.href = "https://markprodamus.link.sendsay.ru/markprodamus/8000,=0AHnUPebI4Nx2kympCrHPBw/8930,2543389,140017,?aHR0cHM6Ly9wdXNzZXJyLnBheWZvcm0ucnU=";
+});
